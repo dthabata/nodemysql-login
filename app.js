c2082f1403f23c005e68888ab0ba290289975628
dthabata <dthabatita@gmail.com>
2024-01-10 18:59:53 -0300

    feat: connecting to database

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodejs-login'
+});
+
+db.connect((error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MySQL connected...")
+    }
+})
 
 app.get("/", (req, res) => {
     res.send("<h1>Home page</h1>")
