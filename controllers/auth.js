196948dc6e2a737128203aa90a891fe3d4956a5a
dthabata <dthabatita@gmail.com>
2024-01-15 19:02:07 -0300

    feat: token cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,10 +18,37 @@ exports.login = async (req, res) => {
                 message: 'Please provide a valid email and password'
             });
         }
+
+        db.query('SELECT * FROM users where email = ?', [email], async (error, results) => {
+            if (!results || !(await bcrypt.compare(password, results[0].password))) {
+                res.status(401).render('login', {
+                    message: 'Email or password is incorrect'
+                });
+            } else {
+                const id = results[0].id;
+                const token = jwt.sign({ id }, process.send.JWT_SECRET, {
+                    expiresIn: process.env.JWT_EXPIRES_IN
+                });
+
+                console.log('The token is: ' + token);
+
+                const cookieOptions = {
+                    expires: new Date(
+                      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                    ),
+                    httpOnly: true
+                }
+
+                res.cookie('jwt', token, cookieOptions );
+                res.status(200).redirect('/');
+
+                }
+            });
+
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 exports.register = (req, res) => {
     console.log(req.body);
