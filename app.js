39637ec951c36865f8fcc798526224f5e170ed40
dthabata <dthabatita@gmail.com>
2024-01-15 15:01:50 -0300

    feat: sets .env

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: './.env'
+})
 
 const app = express();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodejs-login'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
 });
 
 db.connect((error) => {
