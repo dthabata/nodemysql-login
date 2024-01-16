3c83513564eb8de79620bd0f00fb213e9d4a4005
dthabata <dthabatita@gmail.com>
2024-01-16 12:10:48 -0300

    feat: add user administration to the correct form and brings back the dynamics to profile

--- routes/pages.js
+++ routes/pages.js
@@ -2,8 +2,10 @@ const express = require('express');
 const authController = require('../controllers/auth');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', authController.isLoggedIn, (req, res) => {
+    res.render('index', {
+        user: req.user,
+    });
 });
 
 router.get('/register', (req, res) => {
@@ -16,7 +18,9 @@ router.get('/login', (req, res) => {
 
 router.get('/profile', authController.isLoggedIn, (req, res) => {
     if (req.user) {
-        res.render('profile');
+        res.render('profile', {
+            user: req.user,
+        });
     } else {
         res.redirect('/login');
     }
