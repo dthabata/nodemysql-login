559a508a50c1b8df280dba7ea9beb4444df0a1d4
dthabata <dthabatita@gmail.com>
2024-01-23 16:04:07 -0300

    feat: adds TO DOs for next steps to improve logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -86,6 +86,7 @@ exports.register = (req, res) => {
     });
 };
 
+// TODO: remove cookies logic and use only JWT authorization
 exports.isLoggedIn = async (req, res, next) => {
     if(req.cookies.jwt) {
         try {
@@ -119,6 +120,7 @@ exports.isLoggedIn = async (req, res, next) => {
     }
 };
 
+// TODO: remove cookies logic and use only JWT authorization
 exports.logout = async (req, res) => {
     res.cookie('jwt', 'logout', {
         expires: new Date(Date.now() + 2 * 1000),
