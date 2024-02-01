4223c12e61577d50db0947a1cfc1437f922739e6
dthabata <dthabatita@gmail.com>
2024-02-01 10:51:22 -0300

    feat: authApiMiddleware isLoggedIn for update user

--- src/routes/api-user.js
+++ src/routes/api-user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const apiController = require('../controllers/api-user');
+const authApiMiddleware = require('../controllers/auth-api');
 const router = express.Router();
 
 router.post('/login', apiController.loginApi);
 router.post('/register', apiController.registerApi);
-router.put('/update/:id', apiController.updateApi); // aqui
+router.put('/update/:id', authApiMiddleware.isLoggedIn, apiController.updateApi);
 router.delete('/delete/:id', apiController.deleteApi); // aqui
 
 module.exports = router;
