899e79591efc515d13fa91e62e1812e9b6b59807
dthabata <dthabatita@gmail.com>
2024-02-01 10:01:36 -0300

    fix: syntax change for user router and api

--- src/routes/api-user.js
+++ src/routes/api-user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const apiController = require('../controllers/api');
+const apiController = require('../controllers/api-user');
 const router = express.Router();
 
 router.post('/login', apiController.loginApi);
