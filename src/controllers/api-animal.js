cd85bf6d648131ee9c6242cb4f906eb300af3f02
dthabata <dthabatita@gmail.com>
2024-02-02 10:05:53 -0300

    feat: add pagination to getAnimalPaginatedListApi

--- src/controllers/api-animal.js
+++ src/controllers/api-animal.js
@@ -42,18 +42,17 @@ exports.getAnimalListApi = async (req, res) => {
 exports.getAnimalPaginatedListApi = async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
-    db.query('SELECT * FROM animal ORDER BY id DESC limit 0, 2', (error, results) => {
+    const page = parseInt(req.query.page) || 0;
+    const max = parseInt(req.query.max) || 1;
+    const skip = page * max;
+
+    db.query('SELECT * FROM animal ORDER BY id DESC limit ?, ?', [skip, max], (error, results) => {
 
         if (error) {
             console.log(error);
+            return res.end(JSON.stringify([]));
         } 
-
-        let resp = [];
-        if (results.length > 0) {
-            resp = results;
-        }
-        
-        return res.end(JSON.stringify(resp));
+        return res.end(JSON.stringify(results));
     });
 };
 
