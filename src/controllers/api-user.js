898ae8b146937331c265636f4f1da0f47af27911
dthabata <dthabatita@gmail.com>
2024-02-23 13:53:09 -0300

    fix: corrects loginApi and updateApi training callback hell

--- src/controllers/api-user.js
+++ src/controllers/api-user.js
@@ -11,35 +11,30 @@ const db = mysql.createConnection({
 
 exports.loginApi = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    
+
     try {
         const { email, password } = req.body;
-
         if (!email || !password) {
             res.end(JSON.stringify({ "message": "Falta de email ou senha", "status": false, "token": "" }));
         } else {
-        db.query('SELECT * FROM users where email = ?', [email], (error, results) => {
-            
-            const comparePasswords = () => {
-                bcrypt.compare((password, results[0].password), (err, result) => {
-                    if (err || !result) {
-                        return false;
-                    }
-                    return true;
-                });   
-            }
-
-            if (!results || results.length == 0 || !comparePasswords) {
-                res.status(401).send(JSON.stringify({ "message": "Não encontrou resultados", "status": false, "token": "" }));
-            } else {
-                const id = results[0].id;
-                const name = results[0].name;
-                const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_EXPIRES_IN
-                });
-                res.end(JSON.stringify({ "message": "Ok", "status": true, "token": token, "name": name  }));
-            }
-        });
+            db.query('SELECT * FROM users where email = ?', [email], (error, results) => {
+                if (!results || results.length == 0) {
+                    res.status(401).send(JSON.stringify({ "message": "Não encontrou resultados", "status": false, "token": "" }));
+                } else {
+                    bcrypt.compare(password, results[0].password, (err, result) => {
+                        if (err || !result) {
+                            res.status(401).send(JSON.stringify({ "message": "Não encontrou resultados", "status": false, "token": "" }));
+                        } else {
+                            const id = results[0].id;
+                            const name = results[0].name;
+                            const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+                                expiresIn: process.env.JWT_EXPIRES_IN
+                            });
+                            res.end(JSON.stringify({ "message": "Ok", "status": true, "token": token, "name": name  }));
+                        }    
+                    }); 
+                }
+            });
         }
     } catch (error) {
         console.log(error);
@@ -51,24 +46,24 @@ exports.registerApi = (req, res) => {
 
     const { name, email, password } = req.body;
 
-    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {    
+    db.query('SELECT email FROM users WHERE email = ?', [email], (error, results) => {    
         if (error) {
             console.log(error);
         }
 
         if (results.length > 0) {
             return res.end(JSON.stringify({ "message": "E-mail já em uso", "status": false }));
-        } 
-    
-        const hashedPassword = await bcrypt.hash(password, 8);
+        }
 
-        db.query('INSERT INTO users SET ?', {name: name, email: email, password: hashedPassword}, (error, results) => {
-            if (error) {
-                console.log(error);
-            } else {
-                return res.end(JSON.stringify({ "message": "Usuário cadastrado", "status": true }));
-            }
-        })
+        bcrypt.hash(password, 8, (err, hashedPassword) => {
+            db.query('INSERT INTO users SET ?', {name: name, email: email, password: hashedPassword}, (error, results) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    return res.end(JSON.stringify({ "message": "Usuário cadastrado", "status": true }));
+                }
+            }) 
+        });
     });
 };
 
