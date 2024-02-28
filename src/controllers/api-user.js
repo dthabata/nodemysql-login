effc8d55ff3104ed81450f468d3487d310e4589f
dthabata <dthabatita@gmail.com>
2024-02-28 16:36:38 -0300

    fix: loginApiSync

--- src/controllers/api-user.js
+++ src/controllers/api-user.js
@@ -10,6 +10,7 @@ const db = mysql.createConnection({
     database: process.env.DATABASE,
 });
 
+
 exports.loginApi = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
@@ -44,29 +45,35 @@ exports.loginApi = (req, res) => {
 };
 
 exports.loginApiSync = async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    
-    try {
-        const { email, password } = req.body;
 
-        if (!email || !password) {
-            res.end(JSON.stringify({ "message": "Falta de email ou senha", "status": false, "token": "" }));
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.end(JSON.stringify({ "message": "Falta de email ou senha", "status": false, "token": "" }));
+    } 
+
+    const myQuery = "SELECT * FROM users where email = ?";
+
+    let results = await new Promise((resolve, reject) => db.query(myQuery, [email], (err, results) => {
+        if (err) {
+            reject(err)
         } else {
-        db.query('SELECT * FROM users where email = ?', [email], async (error, results) => {
-            if (!results || results.length == 0 || !(await bcrypt.compare(password, results[0].password))) {               
-                res.status(401).send(JSON.stringify({ "message": "Não encontrou resultados", "status": false, "token": "" }));
-            } else {
-                const id = results[0].id;
-                const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_EXPIRES_IN
-                });
-                res.end(JSON.stringify({ "message": "Ok", "status": false, "token": token }));
-            }
-        });
+            resolve(results);
         }
-    } catch (error) {
-        console.log(error);
-    }
+    }));
+
+    bcrypt.compare(password, results[0].password, (err, result) => {
+        if (err || !result) {
+            res.status(401).send(JSON.stringify({ "message": "Não encontrou resultados", "status": false, "token": "" }));
+        } else {
+            const id = results[0].id;
+            const name = results[0].name;
+            const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+                expiresIn: process.env.JWT_EXPIRES_IN
+            });
+            res.end(JSON.stringify({ "message": "Ok", "status": true, "token": token, "name": name  }));
+        }    
+    }); 
 };
 
 exports.registerApi = (req, res) => {
